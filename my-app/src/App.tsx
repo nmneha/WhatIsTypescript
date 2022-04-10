80df9b42d959f78986a21dafd81526be700c81f0
nusera <nusera.neha@gmail.com>
2022-04-10 00:33:31 -0400

    prettyyyy

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -5,10 +5,11 @@ import { useEffect, useState } from "react";
 import { render } from "@testing-library/react";
 const axios = require('axios');
 
+
 function App() {
 
+  
   const [def, setDef] = useState([]);
-
 
 
   const form: HTMLFormElement = document.querySelector("#defineform");
@@ -18,7 +19,7 @@ function App() {
   });
 
 
-    const word = document.getElementById
+
     useEffect(() => {
       const url = "https://api.dictionaryapi.dev/api/v2/entries/en/";
       form.onsubmit = async () => {
@@ -26,11 +27,11 @@ function App() {
         // console.log(formData);
         const text = formData.get("defineword") as string;
         axios.get(url + text)
-          .then((res) => {
+          .then((res: any) => {
             console.log(res);
             setDef(res.data);
           })
-          .catch((err) => {
+          .catch((err: any) => {
             console.log(err);
         })
         //.then((response: Response) => response.json())
@@ -41,29 +42,39 @@ function App() {
         //  console.log(def);
       }
     }
+    
 
       //.catch((error) => console.log(error));
       //console.log(text);
 
       ,[]);
+
+      
   return (
+   
     <div className="App">
       <main className="container">
 <div className="bg-light p-5 rounded">
   <img src="https://www.publicbooks.org/wp-content/uploads/2017/01/book-e1484158615982.jpg" width="250" height="250"></img>
     <h1 className="defined">DICTIONARY</h1>
-    <p className="lead">Defintion</p>
-  {def.map((def) => (
+    <p className="lead">Definition</p>
+  {def.map((def: any) => (
   <ul className="list-unstyled">
     <li><b><u>{def.word}</u></b></li>
-    <li>Phonetic: {def.phonetics[0].text}</li>
-    <li><i>{def.meanings[0].partOfSpeech}</i></li>
-      <ol>
-        <li>{def.meanings[0].definitions[0].definition}</li>
+    {def.phonetics.map((phonetics: any) => (
+  <ul className="list-unstyled">
+    <li><b>Phonetic: </b>{phonetics.text}</li>
+    {def.meanings.map((meanings: any) => (
+  <ul className="list-unstyled">
+    <li><i>{meanings.partOfSpeech}</i></li>
+    <li><b>Antonym(s): </b>{meanings.antonyms}</li>
+    <li><b>Synonym(s): </b>{meanings.synonyms[0]}</li>
+    {meanings.definitions.map((definitions: any) => (
+  <ul className="list-unstyled">
+      <ul>
+        <li>{definitions.definition}</li>
         {/* <li>{def.meanings[0].definitions[1].definition}</li> */}
-      </ol>
-      <li>Antonym(s): {def.meanings[0].antonyms}</li>
-    <li>Synonym(s): {def.meanings[0].synonyms[0]}</li>
+      </ul>
       {/* <li><i>{def.meanings[1].partOfSpeech}</i></li>
       <ol>
         <li>{def.meanings[1].definitions[0].definition}</li>
@@ -71,6 +82,13 @@ function App() {
       </ol>
       <li>Antonym(s): {def.meanings[1].antonyms}</li>
     <li>Synonym(s): {def.meanings[1].synonyms[1]}</li> */}
+    </ul>
+    ))}
+    <p>______________________________________</p>
+    </ul>
+    ))}
+    </ul>
+    ))}
     </ul>
     ))}
  </div>
